2a9498a795365d0c69ba6788f8559ce24db34c50
Koichi Miyamoto <m5813k@gmail.com>
2019-11-03 13:30:54 +0900

    Cue 消すとこまで

--- src/App.tsx
+++ src/App.tsx
@@ -12,14 +12,18 @@ export default function App() {
   return (
     <>
       {time}
-      <svg width={"100vw"} ref={ref}>
+      <svg width={"100vw"} height={500} ref={ref}>
         <CueAdder
           svg={ref.current}
           onAdd={n => setEvents(prev => [...prev, n])}
         />
-        <rect x={time} y={0} width={"1"} height={height * 100} />
+        <rect x={time} y={0} width={1} height={height * 100} />
         {events.map(n => (
-          <Cue key={n} time={n} />
+          <Cue
+            key={n}
+            time={n}
+            onClick={() => setEvents(prev => prev.filter(t => t !== n))}
+          />
         ))}
       </svg>
       <button onClick={() => setTime(0)}>reset</button>
--- src/Cue.tsx
+++ src/Cue.tsx
@@ -1,14 +1,14 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import useStore from "./store";
 
-export default function Cue({ time }) {
+export default function Cue({ time, onClick }) {
   const [{ height }] = useStore();
 
   const w = 1;
   const offset = 4;
   return (
     <>
-      <text x={time} y={20}>
+      <text x={time} y={40}>
         {time}
       </text>
 
@@ -18,8 +18,30 @@ export default function Cue({ time }) {
         width={w + 2 * offset}
         height={height * 100}
         fill="lightgray"
+        onContextMenu={e => {
+          e.preventDefault();
+          return false;
+        }}
       />
       <rect x={time} y={0} width={w} height={height * 100} fill="red" />
+      {/* focus event でなんかしてstoreになげたりする */}
+      <foreignObject x={time} y={40} width={100} height={20}>
+        <input />
+      </foreignObject>
+      <Batsu x={time} y={10} onClick={onClick} />
+    </>
+  );
+}
+function Batsu({ x, y, onClick }) {
+  const r = 10;
+  const mr = 5;
+  return (
+    <>
+      <circle cx={x} cy={y} r={r} fill="red" />
+      <line x1={x - mr} y1={y + mr} x2={x + mr} y2={y - mr} stroke="black" />
+      <line x1={x + mr} y1={y + mr} x2={x - mr} y2={y - mr} stroke="black" />
+
+      <circle cx={x} cy={y} r={r} fill="transparent" onClick={onClick} />
     </>
   );
 }
